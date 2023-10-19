774cf80f447f365660c12e6a9146f3a7e0234a9a
MikusR <mikus.rakis@gmail.com>
2023-10-19 15:45:58 +0000

    UniversityCollection initial structure

--- app/UniversityCollection.php
+++ app/UniversityCollection.php
@@ -4,5 +4,23 @@ namespace App;
 
 class UniversityCollection
 {
+    /**
+     * @var University[]
+     */
+    private array $collection;
 
+    public function __construct(array $universities = [])
+    {
+        $this->collection = $universities;
+    }
+
+    public function add(University $university): void
+    {
+        $this->collection[] = $university;
+    }
+
+    public function list():array
+    {
+        return $this->collection;
+    }
 }
