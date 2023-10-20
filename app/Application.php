cb7d7afc9003c53fa8cb48212446a95ea2906f27
MikusR <mikus.rakis@gmail.com>
2023-10-20 06:57:47 +0000

    wip

--- app/Application.php
+++ app/Application.php
@@ -2,11 +2,36 @@
 
 namespace App;
 
+use PhpSchool\CliMenu\CliMenu;
+use PhpSchool\CliMenu\Builder\CliMenuBuilder;
+
 class Application
 {
     public function run()
     {
 
+        $itemCallable = function (CliMenu $menu) {
+            echo $menu->getSelectedItem()->getText();
+        };
+
+        $menu = (new CliMenuBuilder)
+            ->setTitle('Basic CLI Menu Disabled Items')
+            ->addItem('First Item', $itemCallable)
+            ->addItem('Second Item', $itemCallable, false, true)
+            ->addItem('Third Item', $itemCallable, false, true)
+            ->addSubMenu('Submenu', function (CliMenuBuilder $b) use ($itemCallable) {
+                $b->setTitle('Basic CLI Menu Disabled Items > Submenu')
+                    ->addItem('You can go in here!', $itemCallable);
+            })
+            ->addSubMenu('Disabled Submenu', function (CliMenuBuilder $b) use ($itemCallable) {
+                $b->setTitle('Basic CLI Menu Disabled Items > Disabled Submenu')
+                    ->addItem('Nope can\'t see this!', $itemCallable)
+                    ->disableMenu();
+            })
+            ->addLineBreak('-')
+            ->build();
+
+        $menu->open();
 
     }
 }
