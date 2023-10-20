56c9c9869163402eba3e4845b8e30c883ca93eef
MikusR <mikus.rakis@gmail.com>
2023-10-20 12:31:42 +0000

    wip PHP_Parallel_Lint\PhpConsoleColor

--- app/Application.php
+++ app/Application.php
@@ -2,12 +2,18 @@
 
 namespace App;
 
+use PHP_Parallel_Lint\PhpConsoleColor;
 
 class Application
 {
     public function run()
     {
 
+        $consoleColor = new PhpConsoleColor\ConsoleColor();
+
+        echo "\n";
+        echo $consoleColor->apply(['bold', 'red'], "test\n");
+        echo "test2";
 
 
     }
